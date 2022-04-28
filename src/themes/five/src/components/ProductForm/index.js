65eb47edae0c98e5ab4c3c5f4f7036891245c170
MingXue <mingxue@ordering.co>
2022-04-29 01:49:53 +0800

    stop all video when change slide (#1033)

--- src/themes/five/src/components/ProductForm/index.js
+++ src/themes/five/src/components/ProductForm/index.js
@@ -266,6 +266,18 @@ const ProductOptionsUI = (props) => {
     })
   }
 
+  const handleSlideChange = () => {
+    var videos = document.querySelectorAll('iframe, video')
+    Array.prototype.forEach.call(videos, function (video) {
+      if (video.tagName.toLowerCase() === 'video') {
+        video.pause()
+      } else {
+        var src = video.src
+        video.src = src
+      }
+    })
+  }
+
   return (
     <>
       {props.beforeElements?.map((BeforeElement, i) => (
