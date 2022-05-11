87c85d34bc772910cdd0ee1b5399087298ba29af
dreamsky2020 <59742254+dreamsky2020@users.noreply.github.com>
2022-05-11 23:34:30 +0800

    [Add] recaptcha enable (#1056)

--- src/themes/five/src/components/LoginForm/index.js
+++ src/themes/five/src/components/LoginForm/index.js
@@ -631,6 +631,7 @@ const LoginFormUI = (props) => {
 export const LoginForm = (props) => {
   const loginControllerProps = {
     ...props,
+    isRecaptchaEnable: true,
     UIComponent: LoginFormUI
   }
   return <LoginFormController {...loginControllerProps} />
