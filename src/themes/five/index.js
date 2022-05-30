07e4b7e61a508c2591109c615c4d8fe0d0b4df27
carlosg-ordering <68757729+carlosg-ordering@users.noreply.github.com>
2022-05-30 15:51:31 -0500

    [ADD] missing import (#1094)

--- src/themes/five/index.js
+++ src/themes/five/index.js
@@ -79,9 +79,11 @@ import { UserVerification } from './src/components/UserVerification'
 import { Wallets } from './src/components/Wallets'
 import { WalletTransactionItem } from './src/components/WalletTransactionItem'
 import { BusinessListingSearch } from './src/components/BusinessListingSearch'
+
 import { Button } from './src/styles/Buttons'
 import { Input, TextArea } from './src/styles/Inputs'
 import { Select } from './src/styles/Select'
+import { Checkbox } from '../../styles/Checkbox'
 
 export {
   AccordionDropdown,
@@ -170,5 +172,6 @@ export {
   Button,
   Input,
   TextArea,
-  Select
+  Select,
+  Checkbox
 }
