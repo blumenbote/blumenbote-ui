28d4194be37b814ef4a3cec6adb311978c26f8dc
оло <olo@MacBook-Pro-olo.local>
2022-10-19 11:53:45 +0300

    refactor: global style to components

--- src/contexts/ThemeContext/index.js
+++ src/contexts/ThemeContext/index.js
@@ -1,5 +1,7 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import { createGlobalStyle, css, ThemeProvider as ThemeProviderStyled } from 'styled-components'
+import React, { createContext, useContext, useState } from 'react'
+import { ThemeProvider as ThemeProviderStyled } from 'styled-components'
+
+import { GlobalStyle } from './styles.js'
 
 /**
  * Create ThemeContext
@@ -23,83 +25,6 @@ export const ThemeProvider = ({ children, ...props }) => {
     }
   }
 
-  const GlobalStyle = createGlobalStyle`
-    @media (min-width: 578px) {
-      /** Mozilla scrollbar*/
-      * {
-        scrollbar-color: #CCC !important;
-        scrollbar-width: thin !important;
-      }
-      /** Scrollbar for browser based on webkit */
-      ::-webkit-scrollbar {
-        width: 6px;
-        height: 0px;
-      }
-      ::-webkit-scrollbar-thumb {
-        background: #CCCCCC;
-      }
-      ::-webkit-scrollbar-thumb:hover {
-        background: #AFAFAF;
-      }
-      ::-webkit-scrollbar-thumb:active {
-        background: #6b6b6b;
-      }
-      ::-webkit-scrollbar-track {
-        background: rgba(204, 204, 204, 0.3);
-      }
-    }
-    body {
-      font-family: '${theme.fonts.primary?.name || 'Helvetica'}', sans-serif;
-      margin: 0;
-      background-color: ${theme.colors.backgroundPage};
-      color: ${getThemeColor()};
-      direction: ltr;
-      ${theme.rtl && css`
-        direction: rtl;
-      `}
-      -webkit-overflow-scrolling: auto;
-    }
-    input, textarea, button {
-      font-family: inherit;
-    }
-    h1,p,span {
-      ${props.isDarkTextColor && css`
-        color: ${theme.colors.darkTextColor}
-      `}
-    }
-    .popup-backdrop {
-      background-color: rgba(0, 0, 0, 0.4);
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      right: 0;
-      z-index: 2000;
-    }
-    .popup-component {
-      background-color: rgba(0, 0, 0, 0.3);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  `
-
-  useEffect(() => {
-    const fonts = Object.entries(theme.fonts || {})
-    fonts.forEach(([name, fontFamily]) => {
-      if (!window.document.getElementById(`${name}-font-styles`)) {
-        const font = window.document.createElement('link')
-        font.id = `${name}-font-styles`
-        font.rel = 'stylesheet'
-        font.async = true
-        font.defer = true
-        font.href = `https://fonts.googleapis.com/css2?family=${fontFamily.name}:wght@${fontFamily.weights.join(';')}&display=swap`
-
-        window.document.body.appendChild(font)
-      }
-    })
-  }, [])
-
   const update = (theme) => {
     setTheme(theme)
   }
@@ -114,7 +39,7 @@ export const ThemeProvider = ({ children, ...props }) => {
   return (
     <ThemeContext.Provider value={[theme, { update, merge }]}>
       <ThemeProviderStyled theme={theme}>
-        <GlobalStyle />
+        <GlobalStyle getThemeColor={getThemeColor} {...props} />
         {children}
       </ThemeProviderStyled>
     </ThemeContext.Provider>
