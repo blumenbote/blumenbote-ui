2846baf93af13c311c389e6b17c1cfe913d99f1b
оло <olo@MacBook-Pro-olo.local>
2022-10-19 11:40:14 +0300

    fix :  missing components

--- index.js
+++ index.js
@@ -1,137 +1,130 @@
 import { AddressDetails } from './src/components/AddressDetails'
 import { AddressForm } from './src/components/AddressForm'
 import { AddressList } from './src/components/AddressList'
-import { BaseComponent } from './src/components/BaseComponent'
+// import { BaseComponent } from './src/components/BaseComponent'
 import { BusinessBasicInformation } from './src/components/BusinessBasicInformation'
 import { BusinessController } from './src/components/BusinessController'
-import { BusinessesList } from './src/components/BusinessesList'
-import { BusinessFeaturedProducts } from './src/components/BusinessFeaturedProducts'
+// import { BusinessesList } from './src/components/BusinessesList'
+import { BusinessesListing } from './src/components/BusinessesListing'
+// import { BusinessFeaturedProducts } from './src/components/BusinessFeaturedProducts'
 import { BusinessInformation } from './src/components/BusinessInformation'
 import { BusinessProductsCategories } from './src/components/BusinessProductsCategories'
-import { BusinessProductsSearch } from './src/components/BusinessProductsSearch'
+// import { BusinessProductsSearch } from './src/components/BusinessProductsSearch'
 import { BusinessReviews } from './src/components/BusinessReviews'
-import { BusinessSortControl } from './src/components/BusinessSortControl'
+// import { BusinessSortControl } from './src/components/BusinessSortControl'
 import { BusinessTypeFilter } from './src/components/BusinessTypeFilter'
 import { Cart } from './src/components/Cart'
 import { Checkout } from './src/components/Checkout'
 import { CouponControl } from './src/components/CouponControl'
-import { DragAndDrop } from './src/components/DragAndDrop'
+// import { DragAndDrop } from './src/components/DragAndDrop'
 import { DriverTips } from './src/components/DriverTips'
-import { ExamineClick } from './src/components/ExamineClick'
-import { FacebookLoginButton } from './src/components/FacebookLoginButton'
+// import { ExamineClick } from './src/components/ExamineClick'
+// import { FacebookLoginButton } from './src/components/FacebookLoginButton'
 import { ForgotPasswordForm } from './src/components/ForgotPasswordForm'
 import { GoogleAutocompleteInput } from './src/components/GoogleAutocompleteInput'
 import { GoogleMaps } from './src/components/GoogleMaps'
-import { GpsButton, GoogleGpsButton } from './src/components/GpsButton'
+// import { GpsButton, GoogleGpsButton } from './src/components/GpsButton'
+// import { GoogleGpsButton } from './src/components/GpsButton'
 import { LanguageSelector } from './src/components/LanguageSelector'
 import { LoginForm } from './src/components/LoginForm'
-import { LogoutAction } from './src/components/LogoutAction'
-import { MainSearch } from './src/components/MainSearch'
-import { MenuControl } from './src/components/MenuControl'
-import { MomentOption } from './src/components/MomentOption'
+// import { LogoutAction } from './src/components/LogoutAction'
+// import { MainSearch } from './src/components/MainSearch'
+// import { MenuControl } from './src/components/MenuControl'
+// import { MomentOption } from './src/components/MomentOption'
 import { MyOrders } from './src/components/MyOrders'
-import { MyOrdersList } from './src/components/MyOrdersList'
-import { OrderList } from './src/components/OrderList'
-import { OrderTypeControl } from './src/components/OrderTypeControl'
+// import { MyOrdersList } from './src/components/MyOrdersList'
+// import { OrderList } from './src/components/OrderList'
+// import { OrderTypeControl } from './src/components/OrderTypeControl'
 import { PaymentOptions } from './src/components/PaymentOptions'
-import { Popup } from './src/components/Popup'
-import { ProductComponent } from './src/components/ProductComponent'
+// import { Popup } from './src/components/Popup'
+// import { ProductComponent } from './src/components/ProductComponent'
 import { ProductForm } from './src/components/ProductForm'
-import { ProductImages } from './src/components/ProductImages'
+// import { ProductImages } from './src/components/ProductImages'
 import { ProductIngredient } from './src/components/ProductIngredient'
 import { ProductOption } from './src/components/ProductOption'
 import { ProductOptionSuboption } from './src/components/ProductOptionSuboption'
-import { ProductsList } from './src/components/ProductsList'
-import { ProductsListing } from './src/components/ProductsListing'
+// import { ProductsList } from './src/components/ProductsList'
+// import { ProductsListing } from './src/components/ProductsListing'
 import { ResetPassword } from './src/components/ResetPassword'
-import { SearchOptions } from './src/components/SearchOptions'
+// import { SearchOptions } from './src/components/SearchOptions'
 import { SignupForm } from './src/components/SignupForm'
-import { SingleBusinessCard } from './src/components/SingleBusinessCard'
-import { SingleOrderCard } from './src/components/SingleOrderCard'
+// import { SingleBusinessCard } from './src/components/SingleBusinessCard'
+// import { SingleOrderCard } from './src/components/SingleOrderCard'
 import { SingleProductCard } from './src/components/SingleProductCard'
 import { SmartAppBanner } from './src/components/SmartAppBanner'
 import { TaxInformation } from './src/components/TaxInformation'
 import { UserDetails } from './src/components/UserDetails'
 import { UserProfileForm } from './src/components/UserProfileForm'
-import { WrapperGoogleMaps } from './src/components/WrapperGoogleMaps'
-import { ConfigContext, ConfigProvider, useConfig } from './src/contexts/ConfigContext'
-import { LanguageContext, LanguageProvider, useLanguage } from './src/contexts/LanguageContext'
-import { OrderContext, OrderProvider, useOrder } from './src/contexts/OrderContext'
-import { SessionContext, SessionProvider, useSession } from './src/contexts/SessionContext'
-import { WebsocketContext, WebsocketProvider, useWebsocket } from './src/contexts/WebsocketContext'
+// import { WrapperGoogleMaps } from './src/components/WrapperGoogleMaps'
+import { ThemeContext, ThemeProvider, useTheme } from './src/contexts/ThemeContext'
+import { AgreementContext, AgreementProvider, useAgreement } from './src/contexts/AgreementContext'
+
 
 export {
   AddressDetails,
   AddressForm,
   AddressList,
-  BaseComponent,
+  // BaseComponent,
   BusinessBasicInformation,
   BusinessController,
-  BusinessesList,
-  BusinessFeaturedProducts,
+  // BusinessesList,
+  // BusinessFeaturedProducts,
   BusinessInformation,
   BusinessProductsCategories,
-  BusinessProductsSearch,
+  // BusinessProductsSearch,
   BusinessReviews,
-  BusinessSortControl,
+  // BusinessSortControl,
   BusinessTypeFilter,
   Cart,
   Checkout,
   CouponControl,
-  DragAndDrop,
+  // DragAndDrop,
   DriverTips,
-  ExamineClick,
-  FacebookLoginButton,
+  // ExamineClick,
+  // FacebookLoginButton,
   ForgotPasswordForm,
   GoogleAutocompleteInput,
   GoogleMaps,
-  GpsButton,
-  GoogleGpsButton,
+  // GpsButton,
+  // GoogleGpsButton,
   LanguageSelector,
   LoginForm,
-  LogoutAction,
-  MainSearch,
-  MenuControl,
-  MomentOption,
+  // LogoutAction,
+  // MainSearch,
+  // MenuControl,
+  // MomentOption,
   MyOrders,
-  MyOrdersList,
-  OrderList,
-  OrderTypeControl,
+  // MyOrdersList,
+  // OrderList,
+  // OrderTypeControl,
   PaymentOptions,
-  Popup,
-  ProductComponent,
+  // Popup,
+  // ProductComponent,
   ProductForm,
-  ProductImages,
+  // ProductImages,
   ProductIngredient,
   ProductOption,
   ProductOptionSuboption,
-  ProductsList,
-  ProductsListing,
+  // ProductsList,
+  // ProductsListing,
   ResetPassword,
-  SearchOptions,
+  // SearchOptions,
   SignupForm,
-  SingleBusinessCard,
-  SingleOrderCard,
+  // SingleBusinessCard,
+  // SingleOrderCard,
   SingleProductCard,
   SmartAppBanner,
   TaxInformation,
   UserDetails,
   UserProfileForm,
-  WrapperGoogleMaps,
+  // WrapperGoogleMaps,
   // Contexts
-  ConfigContext,
-  ConfigProvider,
-  useConfig,
-  LanguageContext,
-  LanguageProvider,
-  useLanguage,
-  OrderContext,
-  OrderProvider,
-  useOrder,
-  SessionContext,
-  SessionProvider,
-  useSession,
-  WebsocketContext,
-  WebsocketProvider,
-  useWebsocket
+  ThemeContext,
+  ThemeProvider,
+  useTheme,
+  AgreementContext,
+  AgreementProvider,
+  useAgreement
 }
+
+console.log('index body')
